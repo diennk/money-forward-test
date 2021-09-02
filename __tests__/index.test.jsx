e9130dbf56bbcced420f690f72153abd6b9fab76
Dien Nguyen <khanhdien@gmail.com>
2021-09-03 00:24:17 +0700

    test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,9 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
 describe('Home', () => {
-    it('renders a heading', async () => {
+    it('renders a heading', () => {
         render(<Home />)
+
+        const heading = screen.getAllByRole('heading', {
+            name: /Accounts Management/i,
+        })
+
+        expect(heading[0]).toBeInTheDocument()
     })
 })
