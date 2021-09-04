7d3d8a216bc685c265cac5299c0f4a606f4b0e54
Dien Nguyen <khanhdien@gmail.com>
2021-09-04 10:29:47 +0700

    test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -5,9 +5,10 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
+import Accounts from '../pages/accounts'
 
 describe('Home', () => {
-    it('renders a heading', () => {
+    it('renders a home', () => {
         render(<Home />)
 
         const heading = screen.getAllByRole('heading', {
@@ -15,5 +16,9 @@ describe('Home', () => {
         })
 
         expect(heading[0]).toBeInTheDocument()
+
+        const linkToPage2 = screen.getByText('Accounts Search')
+
+        expect(linkToPage2).toBeInTheDocument()
     })
 })
